6f000fe4b417149bc5e8079836c59f71069a3b54
Carl de Guia <carl.2795@gmail.com>
2023-11-04 16:17:02 +0800

    feat: setup chakra-ui feat: setup pre-commit tests

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vitemplate
+
+This template provides a fully setup web app that uses the following libraries and frameworks
+
+- Vite
+- ReactJS
+- Chakra UI
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Heading } from '@chakra-ui/react';
+// import { useState } from 'react';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <Container centerContent>
+      <Heading>Vitemplate</Heading>
+    </Container>
+  );
+}
+
+export default App;
